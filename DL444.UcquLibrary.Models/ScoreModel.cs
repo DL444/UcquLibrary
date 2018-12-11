b53721d995d15011551f74d385d0a79c1da03cb1
David Lee <DL444@outlook.com>
2018-12-12 00:00:38 +0800

    Add project files.

---
+++ DL444.UcquLibrary.Models/ScoreModel.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DL444.UcquLibrary.Models
+{
+    public class Score
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int AdmissionYear { get; set; }
+        public string Major { get; set; }
+        public string ManagementClass { get; set; }
+        public double GPA
+        {
+            get
+            {
+                double sumGP = 0;
+                double sumCredit = 0;
+                foreach (Term t in Terms)
+                {
+                    foreach (Course c in t.Courses)
+                    {
+                        double coursePA = c.GPA;
+                        if (coursePA == 0.0) { continue; }
+                        sumGP += c.Credit * coursePA;
+                        sumCredit += c.Credit;
+                    }
+                }
+                if (sumCredit == 0) { return 0; }
+                return sumGP / sumCredit;
+            }
+        }
+        public bool IsMajor { get; set; }
+
+        public List<Term> Terms { get; } = new List<Term>();
+
+        public Score(string id, string name, int admissionYear, string major, string manageClass, bool isMajor)
+        {
+            Id = id;
+            Name = name;
+            AdmissionYear = admissionYear;
+            Major = major;
+            ManagementClass = manageClass;
+            IsMajor = isMajor;
+        }
+        public Score() : this("", "", DateTime.Today.Year, "", "", true) { }
+
+        public double GetGPA(int beginningYear)
+        {
+            // This is different than Term.GPA in that it calculates whole year, not a term.
+            double sumGP = 0;
+            double sumCredit = 0;
+            foreach (Term t in Terms)
+            {
+                if (t.BeginningYear != beginningYear) { continue; }
+                foreach (Course c in t.Courses)
+                {
+                    double coursePA = c.GPA;
+                    if (coursePA == 0.0) { continue; }
+                    sumGP += c.Credit * coursePA;
+                    sumCredit += c.Credit;
+                }
+            }
+            if (sumCredit == 0) { return 0; }
+            return sumGP / sumCredit;
+        }
+
+        public void AddTerm(Term term)
+        {
+            Terms.Add(term);
+        }
+        public bool RemoveTerm(Term term)
+        {
+            return Terms.Remove(term);
+        }
+
+        public void Reverse()
+        {
+            Terms.Sort();
+            Terms.Reverse();
+        }
+
+        public override string ToString()
+        {
+            if(Id == null || Name == null) { return ""; }
+            return $"{Id} {Name} " + (IsMajor ? "主修" : "辅修");
+        }
+    }
+
+    public class Term : IComparable<Term>
+    {
+        public int BeginningYear { get; set; }
+        public int EndingYear { get => BeginningYear + 1; }
+        public int TermNumber { get; set; }
+        public double GPA
+        {
+            get
+            {
+                double sumGP = 0;
+                double sumCredit = 0;
+                foreach (Course c in Courses)
+                {
+                    double coursePA = c.GPA;
+                    if (coursePA == 0.0) { continue; }
+                    sumGP += c.Credit * coursePA;
+                    sumCredit += c.Credit;
+                }
+                if (sumCredit == 0) { return 0; }
+                return sumGP / sumCredit;
+            }
+        }
+
+        public List<Course> Courses { get; } = new List<Course>();
+
+        public Term(int beginYear, int termNumber)
+        {
+            BeginningYear = beginYear;
+            TermNumber = termNumber;
+        }
+        public Term() : this(DateTime.Today.Year, 0) { }
+
+        public void AddCourse(Course subject)
+        {
+            Courses.Add(subject);
+        }
+        public bool RemoveCourse(Course subject)
+        {
+            return Courses.Remove(subject);
+        }
+
+        public override string ToString()
+        {
+            string str = $"{BeginningYear}-{EndingYear}学年第";
+            switch (TermNumber)
+            {
+                case 1: str += "一"; break;
+                case 2: str += "二"; break;
+                case 3: str += "三"; break;
+                default: str += "零"; break;
+            }
+            return str + "学期";
+        }
+
+        public int CompareTo(Term other)
+        {
+            if (other == null) { return 1; }
+            if (this.BeginningYear == other.BeginningYear)
+            {
+                return this.TermNumber - other.TermNumber;
+            }
+            else
+            {
+                return this.BeginningYear - other.BeginningYear;
+            }
+        }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public string SimplifiedName
+        {
+            get
+            {
+                if(string.IsNullOrWhiteSpace(Name)) { return ""; }
+                String[] strs = Name.Split(']');
+                if (strs.Length < 2) { return ""; }
+                else { return strs[1]; }
+            }
+        }
+        public double Credit { get; set; }
+        public string Category { get; set; }
+        public bool IsInitialTake { get; set; }
+        public int Score { get; set; }
+
+        public bool IsMajor { get; set; }
+        public string Comment { get; set; }
+        public string Lecturer { get; set; }
+        public string SimplifiedLecturer
+        {
+            get
+            {
+                if(string.IsNullOrWhiteSpace(Lecturer)) { return ""; }
+                String[] strs = Lecturer.Split(']');
+                if (strs.Length < 2) { return ""; }
+                else { return strs[1]; }
+            }
+        }
+        public DateTime ObtainedTime { get; set; }
+        public double GPA
+        {
+            get
+            {
+                if (Comment == "补考" || IsInitialTake == false || Comment == "缺考" || Comment == "补考(缺考)")
+                {
+                    if (Score >= 60) { return 1.0; }
+                    else { return 0.0; }
+                }
+                if (Score > 90) { return 4.0; }
+                if (Score < 60) { return 0.0; }
+                return 1.0 + 0.1 * (Score - 60);
+            }
+        }
+
+        public Course(string name, double credit, string category, bool isInitialTake, int score, bool isMajor, string comment, string lecturer, DateTime obtainedTime)
+        {
+            Name = name;
+            Credit = credit;
+            Category = category;
+            IsInitialTake = isInitialTake;
+            Score = score;
+            IsMajor = isMajor;
+            Comment = comment;
+            Lecturer = lecturer;
+            ObtainedTime = obtainedTime;
+        }
+        public Course() : this("", 0, "", true, 0, true, "", "", DateTime.Today) { }
+
+        public override string ToString()
+        {
+            return Name ?? "";
+        }
+    }
+}
---
+++ LibraryTest/UnitTest1.cs
@@ -0,0 +1,326 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DL444.UcquLibrary.Models;
+
+namespace LibraryTest
+{
+    [TestClass]
+    public class Test_Course
+    {
+        [TestMethod]
+        public void Test_CourseGPA()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 83, 
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual(3.3, course.GPA, 0.01);
+        }
+        [TestMethod]
+        public void Test_CourseGPA_Full()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 95,
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual(4.0, course.GPA, 0.01);
+        }
+        [TestMethod]
+        public void Test_CourseGPA_Fail()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 59,
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual(0, course.GPA, 0.01);
+        }
+        [TestMethod]
+        public void Test_CourseGPA_Retake()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 83,
+                true, "补考", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual(1.0, course.GPA, 0.01);
+        }
+
+        [TestMethod]
+        public void Test_SimplifiedName()
+        {
+            Course course = new Course("[SE0001]程序设计基础", 2, "Test Category", true, 83,
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual("程序设计基础", course.SimplifiedName);
+        }
+        [TestMethod]
+        public void Test_SimplifiedName_Null()
+        {
+            Course course = new Course(null, 2, "Test Category", true, 83,
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual("", course.SimplifiedName);
+        }
+        [TestMethod]
+        public void Test_SimplifiedName_Empty()
+        {
+            Course course = new Course("   ", 2, "Test Category", true, 83,
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual("", course.SimplifiedName);
+        }
+        [TestMethod]
+        public void Test_SimplifiedName_Nonstandard()
+        {
+            Course course = new Course("Something Nasty", 2, "Test Category", true, 83,
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual("", course.SimplifiedName);
+        }
+
+        [TestMethod]
+        public void Test_SimplifiedLecturer()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 83,
+                true, "", "[44444]David Lee", System.DateTime.Today);
+            Assert.AreEqual("David Lee", course.SimplifiedLecturer);
+        }
+        [TestMethod]
+        public void Test_SimplifiedLecturer_Null()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 83,
+                true, "", null, System.DateTime.Today);
+            Assert.AreEqual("", course.SimplifiedLecturer);
+        }
+        [TestMethod]
+        public void Test_SimplifiedLecturer_Empty()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 83,
+                true, "", "  ", System.DateTime.Today);
+            Assert.AreEqual("", course.SimplifiedLecturer);
+        }
+        [TestMethod]
+        public void Test_SimplifiedLecturer_Nonstandard()
+        {
+            Course course = new Course("Course", 2, "Test Category", true, 83,
+                true, "", "Lecturer", System.DateTime.Today);
+            Assert.AreEqual("", course.SimplifiedLecturer);
+        }
+
+        [TestMethod]
+        public void Test_DefaultConstructor()
+        {
+            Course course = new Course();
+            Assert.AreEqual("", course.SimplifiedName);
+            Assert.AreEqual("", course.SimplifiedLecturer);
+            Assert.AreEqual(0, course.GPA);
+            Assert.AreEqual("", course.ToString());
+        }
+    }
+
+    [TestClass]
+    public class Test_Term
+    {
+        [TestMethod]
+        public void Test_TermGPA()
+        {
+            Course[] courses = new Course[]
+            {
+                new Course("Course", 1, "Test Category", true, 83,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 2, "Test Category", true, 96,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 42,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 4, "Test Category", true, 67,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 72,
+                    true, "补考", "Lecturer", System.DateTime.Today)
+
+            };
+
+            Term term = new Term(2018, 1);
+            foreach(Course c in courses)
+            {
+                term.AddCourse(c);
+            }
+
+            Assert.AreEqual(2.11, term.GPA, 0.01);
+        }
+        [TestMethod]
+        public void Test_TermGPA_Empty()
+        {
+            Term term = new Term();
+            Assert.AreEqual(0, term.GPA, 0.01);
+        }
+
+        [TestMethod]
+        public void Test_TermCompareTo()
+        {
+            Assert.AreEqual(1, new Term(2018, 1).CompareTo(new Term(2017, 2)));
+            Assert.AreEqual(-1, new Term(2017, 1).CompareTo(new Term(2017, 2)));
+            Assert.AreEqual(0, new Term(2018, 1).CompareTo(new Term(2018, 1)));
+        }
+        [TestMethod]
+        public void Test_TermCompareTo_Null()
+        {
+            Assert.AreEqual(1, new Term().CompareTo(null));
+        }
+
+        [TestMethod]
+        public void Test_TermToString()
+        {
+            Term term = new Term(2018, 2);
+            Assert.AreEqual("2018-2019学年第二学期", term.ToString());
+            term = new Term(2017, 1);
+            Assert.AreEqual("2017-2018学年第一学期", term.ToString());
+            term = new Term(2019, 3);
+            Assert.AreEqual("2019-2020学年第三学期", term.ToString());
+            term = new Term(2019, 10);
+            Assert.AreEqual("2019-2020学年第零学期", term.ToString());
+        }
+    }
+
+    [TestClass]
+    public class Test_Score
+    {
+        [TestMethod]
+        public void Test_ScoreGPA()
+        {
+            Course[] courses1 = new Course[]
+            {
+                new Course("Course", 1, "Test Category", true, 83,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 2, "Test Category", true, 96,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 42,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 4, "Test Category", true, 67,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 72,
+                    true, "补考", "Lecturer", System.DateTime.Today)
+            };
+            // 2.11
+
+            Term term1 = new Term(2018, 1);
+            foreach (Course c in courses1)
+            {
+                term1.AddCourse(c);
+            }
+
+            Course[] courses2 = new Course[]
+            {
+                new Course("Course", 2, "Test Category", true, 83,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 96,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 1, "Test Category", true, 42,
+                    true, "", "Lecturer", System.DateTime.Today),
+            };
+            // 3.72
+
+            Term term2 = new Term(2017, 3);
+            foreach (Course c in courses2)
+            {
+                term2.AddCourse(c);
+            }
+
+            Score score = new Score("20444444", "DL444", 2044, "SE", "SE01", true);
+            score.AddTerm(term1);
+            score.AddTerm(term2);
+
+            Assert.AreEqual(2.65, score.GPA, 0.01);
+        }
+        [TestMethod]
+        public void Test_ScoreGPA_Empty()
+        {
+            Score score = new Score();
+            Assert.AreEqual(0, score.GPA, 0.01);
+        }
+        [TestMethod]
+        public void Test_Score_GPA_Year()
+        {
+            Course[] courses1 = new Course[]
+            {
+                new Course("Course", 1, "Test Category", true, 83,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 2, "Test Category", true, 96,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 42,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 4, "Test Category", true, 67,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 72,
+                    true, "补考", "Lecturer", System.DateTime.Today)
+            };
+            // 2.11
+
+            Term term1 = new Term(2018, 1);
+            foreach (Course c in courses1)
+            {
+                term1.AddCourse(c);
+            }
+
+            Course[] courses2 = new Course[]
+            {
+                new Course("Course", 2, "Test Category", true, 83,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 96,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 1, "Test Category", true, 42,
+                    true, "", "Lecturer", System.DateTime.Today),
+            };
+            // 3.72
+
+            Term term2 = new Term(2018, 3);
+            foreach (Course c in courses2)
+            {
+                term2.AddCourse(c);
+            }
+
+            Score score = new Score("20444444", "DL444", 2044, "SE", "SE01", true);
+            score.AddTerm(term1);
+            score.AddTerm(term2);
+
+            Assert.AreEqual(2.65, score.GetGPA(2018), 0.01);
+            term2.BeginningYear = 2017;
+            Assert.AreEqual(2.11, score.GetGPA(2018), 0.01);
+        }
+
+        [TestMethod]
+        public void Test_ScoreReverse()
+        {
+            Course[] courses1 = new Course[]
+            {
+                new Course("Course", 1, "Test Category", true, 83,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 2, "Test Category", true, 96,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 42,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 4, "Test Category", true, 67,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 72,
+                    true, "补考", "Lecturer", System.DateTime.Today)
+            };
+            // 2.11
+
+            Term term1 = new Term(2018, 1);
+            foreach (Course c in courses1)
+            {
+                term1.AddCourse(c);
+            }
+
+            Course[] courses2 = new Course[]
+            {
+                new Course("Course", 2, "Test Category", true, 83,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 3, "Test Category", true, 96,
+                    true, "", "Lecturer", System.DateTime.Today),
+                new Course("Course", 1, "Test Category", true, 42,
+                    true, "", "Lecturer", System.DateTime.Today),
+            };
+            // 3.72
+
+            Term term2 = new Term(2017, 3);
+            foreach (Course c in courses2)
+            {
+                term2.AddCourse(c);
+            }
+
+            Score score = new Score("20444444", "DL444", 2044, "SE", "SE01", true);
+            score.AddTerm(term1);
+            score.AddTerm(term2);
+
+            score.Reverse();
+            Assert.AreSame(term1, score.Terms[0]);
+            Assert.AreSame(term2, score.Terms[1]);
+        }
+    }
+}
